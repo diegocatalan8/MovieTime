8e726436ba9187ee35b24205b1d8aef9b9562012
Diego Catalan <diegocatalan.sdev@gmail.com>
2021-03-26 17:24:50 -0600

    Creamos la navegacion de pantallas en el Drawer

--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+
 import {View} from "react-native";
 import {DrawerContentScrollView} from "@react-navigation/drawer";
 import {Drawer, Switch, TouchableRipple, Text} from "react-native-paper";
+import { NavigationHelpersContext } from "@react-navigation/core";
 
 export default function DrawerContent(props){
-    console.log(props);
+    
     const {navigation} = props;
+    const [active, setActive] = useState("home");
+    const onChangeScreen = (screen) =>{
+        setActive(screen);
+        navigation.navigate(screen);
+    }
     
     return(  
         
             <DrawerContentScrollView>
                         <Drawer.Section>
-                            <Drawer.Item label="Inicio" onPress={()=> navigation.navigate("news")}/>
+                            <Drawer.Item label="Inicio" active={active === "home"} onPress={()=>onChangeScreen("home")} />
+                            <Drawer.Item label="Peliculas Populares" active={active === "popular"}  onPress={()=>onChangeScreen("popular")}/>
+                            <Drawer.Item label="Nuevas Peliculas" active={active === "news"} onPress={()=>onChangeScreen("news")}/>
                         </Drawer.Section>
             </DrawerContentScrollView>
         
