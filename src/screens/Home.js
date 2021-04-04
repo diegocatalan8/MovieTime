5fa735450595d9ffe299d29f720cc755720094f7
Diego Catalan <diegocatalan.sdev@gmail.com>
2021-04-04 15:56:54 -0600

    Se pasaron los datos escritos del Componente Movie al componente Home - Fue una confusion

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,28 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { useEffect } from "react";
+import { useState } from "react";
+import {View, Text, ScrollView, StyleSheet} from "react-native";
+import {getNewsMoviesApi} from "../api/movies"
 
 export default function Home(){
-    return(
-        <View>
-        <Text>Estamos en HOME</Text>
-        </View>
-    );
+    //Creamos un estado para obtener las peliculas
+    const [newMovies, setMovies] = useState(null);
+    //usamos useEfect
+    useEffect(()=>{
+        getNewsMoviesApi().then((response)=>{
+            setMovies(response.result);
+        });
+    });
+
+    //retornamos el componente
+   return( 
+       //componente para que la pantalla sea con escroll y el atributo indica que el escroll sea invicible
+            <ScrollView showsVerticalScrollIndicator={false}>
+
+            </ScrollView>
+);
 }
+
+const style = StyleSheet.create({
+
+});
--- src/screens/Movie.js
+++ src/screens/Movie.js
@@ -1,28 +1,18 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
 import {View, Text, ScrollView, StyleSheet} from "react-native";
 import {getNewsMoviesApi} from "../api/movies"
 
 export default function Movie(){
-    //Creamos un estado para obtener las peliculas
-    const [newMovies, setMovies] = useState(null);
-    //usamos useEfect
-    useEffect(()=>{
-        getNewsMoviesApi().then((response)=>{
-            setMovies(response.result);
-        });
-    });
+    
 
-    //retornamos el componente
+    
    return( 
-       //componente para que la pantalla sea con escroll y el atributo indica que el escroll sea invicible
-            <ScrollView showsVerticalScrollIndicator={false}>
-
-            </ScrollView>
+   <View>
+        <Text>
+        ESTAMOS EN MOVIE
+        </Text>
+   </View>
+           
 );
 }
 
-const style = StyleSheet.create({
-
-});
