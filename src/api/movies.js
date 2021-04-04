274a8ed7289446ffce11c6406be342ef34ccb18e
Diego Catalan <diegocatalan.sdev@gmail.com>
2021-04-04 14:55:22 -0600

    Se creo la funcion de peticion para las peliculas nuevas en el archivo movies, y los hooks de retornos en el archivo movie.

---
+++ src/api/movies.js
@@ -0,0 +1,15 @@
+import {API_HOST, API_KEY, LANG} from "../utils/constants";
+
+
+//peticion para obtener las peliculas nuevas
+export function getNewsMoviesApi(page = 1){
+        const url =`${API_HOST}/movie/now_playing?api_key=${API_KEY}&language=${LANG}&page=${page}`;
+
+        return fetch(url)
+        .then((response)=>{
+                return responese.json();
+        })
+        .then((result)=>{
+            return result;
+        })
+}
--- src/screens/Movie.js
+++ src/screens/Movie.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import {View, Text} from "react-native";
+import {getNewsMoviesApi} from "../api/movies"
 
 export default function Movie(){
+    //Creamos un estado para obtener las peliculas
+    const [newMovies, setMovies] = useState(null);
+    //usamos useEfect
+    useEffect(()=>{
+        getNewsMoviesApi().then((response)=>{
+            setMovies(response.result);
+        });
+    });
+
+    //retornamos el componente
    return( 
 <View>
     <Text>Estamos en MOVIE</Text>
