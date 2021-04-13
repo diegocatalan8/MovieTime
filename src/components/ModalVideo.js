e5a69fd4723a16cab68eca83d72c9bc378ad89f6
Diego Catalan <diegocatalan.sdev@gmail.com>
2021-04-13 14:00:54 -0600

    Se instalo una dependencia para renderizar videos en android, y se agrego el componente WebView en ModalVideo creando una condicion de renderizacion de video en IOS Y ANDROID

--- src/components/ModalVideo.js
+++ src/components/ModalVideo.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from "react";
-import {StyleSheet} from "react-native";
+import {StyleSheet, Platform} from "react-native";
 import {Modal, IconButton, Title} from "react-native-paper";
 import YouTube from "react-native-youtube";
+import { WebView } from 'react-native-webview';
 import {getVideoMovieApi} from "../api/movies"
 
 export default function ModalVideo(props){
@@ -24,7 +25,12 @@ export default function ModalVideo(props){
 
     return(
         <Modal visible={show} contentContainerStyle={styles.modal}>
-                <YouTube videoId={video} style={styles.video}/>
+        
+        {//Condicion para renderizar el video en android
+            Platform.OS === "ios" ? ( <YouTube videoId={video} style={styles.video}/>) :
+            (<WebView style={{width:500}} source={{uri:`https://www.youtube.com/embed/${video}?controls=0&showinfo=0`}}/>)
+    }
+               
                 <IconButton
                 icon="close"
                 onPress={()=>setShow(false)}
