fae6c2570c909f649fc5db21c7b8c24cd791101b
Diego Catalan <diegocatalan.sdev@gmail.com>
2021-05-01 01:35:49 -0600

    Se pinto la imagen de las peliculas en popular.js

--- src/screens/Popular.js
+++ src/screens/Popular.js
@@ -1,20 +1,78 @@
 import React, {useState, useEffect} from "react";
-import {View, Text} from "react-native";
+import {StyleSheet, ScrollView, View, Image} from "react-native";
+import {Text} from "react-native-paper";
+import {map} from "lodash";
 import {getPopularMoviesApi} from "../api/movies";
+import {BASE_PATH_IMG} from "../utils/constants"
+import {noImage} from "../assets/jpg/default.jpeg";
 
 
 export default function Popular(props){
     const {navigatio} = props;
     const [movies, setMovies] = useState(null);
+    
+
+
+    
     useEffect(()=>{
         getPopularMoviesApi(1).then((response)=>{
             setMovies(response.results);
         })
 
     }, [])
+
+    
     return(
-        <View>
-            <Text>Estamos en POPULAR</Text>
-    </View>
+        <ScrollView>
+       
+            {map(movies, (movie, index)=>(
+                <Movie key={index} movie={movie} />  
+               
+            ))}
+          
+        </ScrollView>
     );
 }
+
+function Movie(props){
+    
+  
+    const {movie} = props;
+    const {poster_path}=movie;
+
+
+    
+    return(
+        <View style={styles.movie}>
+                <View style={styles.left}>
+                    <Image
+                    style={styles.image}
+                    source={poster_path ? {uri:`${BASE_PATH_IMG}/w500${poster_path}`}:noImage}
+                    />
+                </View>
+
+                <View>
+                    <Text>right</Text>
+                </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+    
+    movie: {
+        marginBottom: 20,
+        flexDirection:"row",
+        alignItems:"center",
+    },
+    left:{
+        marginRight:20,
+
+    },
+    image:{
+        width:100,
+        height:150,
+    }
+}
+);
