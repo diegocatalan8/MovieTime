d9f30561f787dd32c09e5de1d981ca7652a0a3a5
Diego Catalan <diegocatalan.sdev@gmail.com>
2021-05-01 17:06:10 -0600

    Se renderizaron los nombres de las peliculas en News.js screen

--- src/screens/News.js
+++ src/screens/News.js
@@ -1,11 +1,37 @@
-import React from "react";
-import {View, Text} from "react-native";
+import React, {useState, useEffect} from "react";
+import {StyleSheet, View, ScrollView, Dimensions, Image, TouchableWithoutFeedback} from "react-native";
+import {Button, Text} from "react-native-paper";
+import {map} from "lodash";
+import {getNewsMoviesApi} from "../api/movies"
 
-export default function News(){
+
+export default function News(props){
+   const {navigation} = props;
+   const [movies, setMovies] = useState(null);
+   const [page, setPage] = useState(1);
+
+   useEffect(()=>{
+      getNewsMoviesApi(page).then((response)=>{
+        setMovies(response.results);
+      })
+   }, []);
+
+
    return(
-<View>
-    <Text>Estamos en NEWS</Text>
-</View>
+         <ScrollView>
+                  {map(movies, (movie, index)=>(
+                     <Movie key={index} movie={movie}/>) 
+                  )}
+         </ScrollView>
+   );
+}
 
+function Movie(props){
+   const {movie}= props;
+   const {title}=movie;
+   return(
+         <View>
+            <Text>{title}</Text>
+         </View>
    );
 }
