4a79b60adad7df70169ae9f7b46850c5ff4b3e02
Diego Catalan <diegocatalan.sdev@gmail.com>
2021-05-01 16:16:03 -0600

    Terminamos el modulo de peliculas populares en el screen pupular.js

--- src/screens/Popular.js
+++ src/screens/Popular.js
@@ -1,45 +1,78 @@
 import React, {useState, useEffect} from "react";
-import {StyleSheet, ScrollView, View, Image} from "react-native";
-import {Text, Title} from "react-native-paper";
+import {StyleSheet, ScrollView, View, Image, TouchableWithoutFeedback} from "react-native";
+import {Text, Title, Button} from "react-native-paper";
 import {map} from "lodash";
-import {getPopularMoviesApi} from "../api/movies";
+import {getPopularMoviesApi, getVideoMovieApi} from "../api/movies";
 import {BASE_PATH_IMG} from "../utils/constants"
 import {noImage} from "../assets/jpg/default.jpeg";
+import usePreferences from "../hooks/usePreferences"
 
 
 export default function Popular(props){
-    const {navigatio} = props;
+    const {navigation} = props;
+    const {theme} =usePreferences();
+    console.log(theme);
     const [movies, setMovies] = useState(null);
+    const [showBtnMore, setShowBtnMore] = useState(true);
+    const [page, setPage] = useState(1);
     
 
 
     
     useEffect(()=>{
-        getPopularMoviesApi(1).then((response)=>{
-            setMovies(response.results);
+        getPopularMoviesApi(page).then((response)=>{
+            const totalPages = response.total_pages;
+            if(page<totalPages){
+                if(!movies){
+                    setMovies(response.results);
+                }else{
+                    setMovies([...movies, ...response.results ]);
+                }
+            }else{
+                setShowBtnMore(false);
+            }
         })
 
-    }, [])
+    }, [page])
 
     
     return(
+       
         <ScrollView>
        
             {map(movies, (movie, index)=>(
-                <Movie key={index} movie={movie} />  
+                <Movie key={index} movie={movie} theme={theme} navigation = {navigation}/>  
             ))}
+            {showBtnMore &&(
+                <Button
+                mode="contained"
+                contentStyle={styles.loadMoreContainer}
+                style={styles.loadMore}
+                labelStyle={{color: theme === "dark" ? "#fff" : "#000"}}
+                onPress={()=>{
+                    setPage(page+1);
+
+                }}
+                >
+                    Cargar mas...
+                </Button>
+            )}
           
         </ScrollView>
+       
     );
 }
 
 function Movie(props){
-    const {movie} = props;
-    const {poster_path, title, release_date, vote_count, vote_average}=movie;
+    const {movie, navigation} = props;
+    const {id, poster_path, title, release_date, vote_count, vote_average}=movie;
 
-
+   const goMovie = () => {
+       navigation.navigate("movie",{id});
+   }
     
     return(
+        <TouchableWithoutFeedback onPress={goMovie}>
         <View style={styles.movie}>
                 <View style={styles.left}>
                     <Image
@@ -54,6 +87,7 @@ function Movie(props){
                     <Text>{vote_count} votos</Text>
                 </View>
         </View>
+        </TouchableWithoutFeedback>
     );
 }
 
@@ -72,6 +106,13 @@ const styles = StyleSheet.create({
     image:{
         width:100,
         height:150,
+    },
+    loadMoreContainer:{
+        paddingTop:10,
+        paddingBottom:30,
+    },
+    loadMore:{
+        backgroundColor:"transparent",
     }
 }
 );
