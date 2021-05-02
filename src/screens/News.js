398dc21b26f93ab40bc2e2abfa438685d7ecf31a
Diego Catalan <diegocatalan.sdev@gmail.com>
2021-05-02 00:51:09 -0600

    Se termino la funcionalidad de el screen News.js

--- src/screens/News.js
+++ src/screens/News.js
@@ -4,6 +4,7 @@ import {Button, Text} from "react-native-paper";
 import {map} from "lodash";
 import {getNewsMoviesApi} from "../api/movies"
 import {BASE_PATH_IMG} from "../utils/constants"
+import usePreferences from "../hooks/usePreferences"
 
 const {width} = Dimensions.get("window");
 
@@ -11,12 +12,23 @@ export default function News(props){
    const {navigation} = props;
    const [movies, setMovies] = useState(null);
    const [page, setPage] = useState(1);
+   const [showBtnMore, setShowBtnMore] = useState(true);
+   const {theme}= usePreferences();
 
    useEffect(()=>{
       getNewsMoviesApi(page).then((response)=>{
-        setMovies(response.results);
+         const totalPages = response.total_pages;
+         if(page<totalPages){
+             if(!movies){
+                 setMovies(response.results);
+             }else{
+                 setMovies([...movies, ...response.results ]);
+             }
+         }else{
+             setShowBtnMore(false);
+         }
       })
-   }, []);
+   }, [page]);
 
    
 
@@ -25,30 +37,48 @@ export default function News(props){
          <ScrollView >
          <View style={styles.container}>
                   {map(movies, (movie, index)=>(
-                     <Movie key={index} movie={movie}/>) 
+                     <Movie key={index} movie={movie} navigation={navigation}/>) 
                   )}
          </View>
+         {showBtnMore && (
+            <Button
+            mode="contained"
+            contentStyle={styles.loadMoreContainer}
+            style={styles.loadMore}
+            labelStyle={{color: theme === "dark" ? "#fff" : "#000"}}
+            onPress={()=> setPage(page+1)}
+            >
+               Cargar mas...
+            </Button>
+         )}
          </ScrollView>
    );
 }
 
 function Movie(props){
-   const {movie}= props;
+   const {movie, navigation}= props;
    const {id, title, poster_path}=movie;
+
+   const goMovie = () =>{
+      navigation.navigate("movie", {id});
+   }
    return(
-         <View style={styles.movie}>
-            {poster_path ? 
-               (
-            <Image
-            style={styles.image}
-            source={{ uri:`${BASE_PATH_IMG}/w500${poster_path}`}} />
-            ) 
-            :
-            (
-               <Text>{title}</Text>
-            )
-         }
-         </View>
+      <TouchableWithoutFeedback onPress={goMovie}>
+      <View style={styles.movie}>
+      {poster_path ? 
+         (
+      <Image
+      style={styles.image}
+      source={{ uri:`${BASE_PATH_IMG}/w500${poster_path}`}} />
+      ) 
+      :
+      (
+         <Text>{title}</Text>
+      )
+   }
+   </View>
+      </TouchableWithoutFeedback>
+         
    );
 }
 
@@ -59,14 +89,22 @@ const styles = StyleSheet.create({
       flexWrap: "wrap",
    },
    movie:{
-      width: width / 3,
-      height:200,
+      width: width / 2,
+      height:300,
       justifyContent:"center",
       alignItems:"center",
    },
    image:{
       width: "100%",
       height: "100%",
+   },
+   loadMoreContainer:{
+      paddingTop:10,
+      paddingBottom:30,
+   },
+   loadMore:{
+      backgroundColor:"transparent",
+
    }
 
 });
